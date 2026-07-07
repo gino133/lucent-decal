47521fbd899ab30b07d5970e7dc767bc315e4c18
gino133 <79187467+gino133@users.noreply.github.com>
2026-07-07 07:45:30 +0700

    Delete backend directory

--- backend/routes/menu.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getMenu, updateMenu } = require('../controllers/menuController');
-const auth = require('../middleware/auth');
-
-router.get('/:name', getMenu);
-router.post('/', auth, updateMenu);
-
-module.exports = router;
--- backend/routes/settings.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getSetting, updateSetting } = require('../controllers/settingController');
-const auth = require('../middleware/auth');
-
-router.get('/:key', getSetting);
-router.post('/', auth, updateSetting);
-
-module.exports = router;
